6b88b4bbad644365791ca64b5a505d8574ff1da9
dolivare5 <deiverolivares2@gmail.com>
2022-04-06 11:30:50 -0500

    Imagenes minificadas gon gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 const {  src, dest, watch, series, parallel } = require('gulp');
+
+// CSS y SASS
 const sass = require('gulp-sass') (require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+// Imagenes
+
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css (done){
     // Compilar SASS
@@ -16,14 +24,43 @@ function css (done){
     done();
 }
 
+function imagenes(done){
+    src('src/img/**/*')
+        .pipe( imagemin({ optimizationLevel: 3 }) )
+        .pipe( dest('build/img') );
+    done();
+}
+
+function versionWebp( ){
+    const opciones = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe( dest('build/img') )
+}
+
+function versionAvif() {
+    const opciones = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img') );
+}
+
 function dev (){
     watch( 'src/scss/**/*.scss', css);
+    watch( 'src/img/**/*', imagenes);
 }
 
 
 exports.css = css;
+exports.imagenes = imagenes;
 exports.dev = dev;
-exports.default = parallel(css, dev);
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series( imagenes, versionWebp, versionAvif, css, dev);
 
 // Series
 
