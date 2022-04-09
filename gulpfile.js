2d57c555501e188e1d74cab8ca0b23a6d05b7cf1
dolivare5 <deiverolivares2@gmail.com>
2022-04-08 21:49:21 -0500

    Agruegue la libreria de cssnano al proyecto.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const sass = require('gulp-sass') (require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
 
 // Imagenes
 
@@ -21,7 +22,7 @@ function css (done){
     src('src/scss/app.scss')
         .pipe( sourcemaps.init() )
         .pipe( sass())
-        .pipe( postcss([ autoprefixer() ]) )
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
         .pipe( sourcemaps.write('.') )
         .pipe( dest('build/css') );
     done();
